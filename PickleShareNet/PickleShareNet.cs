a9053a747d7ebcc63ca2e6141f4842a5a00ef0c5
Ville M. Vainio <ville.vainio@basware.com>
2018-01-07 16:05:45 +0200

    add content

---
+++ PickleShareNet/PickleShareNet.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace PickleShareNet
+{
+
+    static class FileHelpers
+    {
+        public static string SanitizeFileName(string filename) =>
+            string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+
+        public static string EnsureDir(string path)
+        {
+            var full = Path.GetFullPath(path);
+            if (!Directory.Exists(full))
+            {
+                Directory.CreateDirectory(full);
+            }
+            return full;
+        }
+
+        public static string SafeRead(string path)
+        {
+            int counter = 0;
+            while (counter < 100)
+            {
+                try
+                {
+                    var txt = File.ReadAllText(path);
+                    return txt;
+                }
+                catch (IOException e)
+                {
+                    Thread.Sleep(1);
+                    counter++;
+                }
+            }
+            return null;
+        }
+        public static bool SafeWrite(string path, string cont)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    File.WriteAllText(path, cont);
+                    return true;
+
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    EnsureDir(Path.GetDirectoryName(path));
+                }
+
+                catch (IOException e)
+                {
+                    if (e.HResult != -2147024864)
+                    {
+                        throw;
+                    }
+                    return false;
+
+                }
+            }
+        }
+
+
+    }
+    public class PickleShareDb
+    {
+        string _root;
+        public PickleShareDb(string root)
+        {
+            _root = FileHelpers.EnsureDir(root);
+
+        }
+
+        private string PathTo(string tail)
+        {
+            return Path.Combine(_root, tail);
+        }
+        private string PathToId(string tail, string id)
+        {
+            var dirName = FileHelpers.EnsureDir(Path.Combine(_root, tail));
+
+            return Path.Combine(dirName, id);
+        }
+        public (T value, bool ok) TryGet<T>(string path)
+        {
+            var fname = Path.Combine(_root, path);
+            if (!File.Exists(fname))
+            {
+                return (default(T), false);
+            }
+            var cont = FileHelpers.SafeRead(fname);
+            var val = JsonConvert.DeserializeObject<T>(cont);
+            return (val, true);
+        }
+
+        public bool Set(string path, object val)
+        {
+            var fname = PathTo(path);
+            var serialized = JsonConvert.SerializeObject(val);
+            return FileHelpers.SafeWrite(fname, serialized);
+        }
+
+        public bool SetByType<T>(string id, T val)
+        {
+            var pathFromType = FileHelpers.SanitizeFileName(typeof(T).Name);
+            var tgt = PathToId(pathFromType, id);
+            return Set(tgt, val);
+        }
+        public (T value, bool ok) GetByType<T>(string id)
+        {
+            var pathFromType = FileHelpers.SanitizeFileName(typeof(T).Name);
+            var tgt = PathToId(pathFromType, id);
+            return TryGet<T>(tgt);
+        }
+
+        public string[] Keys(string path) => Directory.GetFiles(PathTo(path)).Select(p => path + "/" + Path.GetFileName(p)).ToArray();
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,68 @@
+﻿using PickleShareNet;
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace PickleShareNet.Test
+{
+    class TestType
+    {
+        public string foo;
+        public string[] bar;
+
+    }
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        [Ignore("only for manual stress testing")]
+        public static void Stress()
+        {
+            var db = new PickleShareDb("c:/t/.testdb");
+            var testArr = Enumerable.Repeat(0xdeadbeef, 10000).ToArray();
+            int counter = 0;
+            while (true)
+            {
+                var testid = $"longobject{counter % 4}";
+                db.Set(testid, testArr);
+                var (o, ok) = db.TryGet<uint[]>(testid);
+                counter++;
+                if (counter % 1000 == 0)
+                {
+                    Console.Write($"{counter},");
+                }
+            }
+        }
+
+        [Test]
+        public static void SimpleTest()
+        {
+            var db = new PickleShareDb("c:/t/.testdb");
+
+           
+            var dummytype = new
+            {
+                foo = 11,
+                bar = 12
+
+            };
+            db.Set("test1", dummytype);
+            db.SetByType("12", dummytype);
+            var tt = new TestType
+            {
+                foo = "hello",
+                bar = new[] { "world", "again" }
+            };
+            db.SetByType("13", tt);
+            var (gotval, ok) = db.GetByType<TestType>("13");
+            Assert.IsTrue(gotval.foo == "hello");
+
+            db.Set("long/path", 11);
+            var (r, ok2) = db.TryGet<int>("long/path");
+            Assert.IsTrue(ok2);
+            Assert.IsTrue(r == 11);
+            var keys = db.Keys("long");
+            Assert.IsTrue(keys.Length == 1 && keys[0] == "long/path");
+        }
+    }
+}
