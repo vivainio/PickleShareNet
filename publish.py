9d95750f785120922c0713952b39310a0b865e92
Ville M. Vainio <ville.vainio@basware.com>
2018-01-07 16:29:58 +0200

    more packaging

--- publish.py
+++ publish.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 
-import os
+import os,shutil
 
 def c(s):
     print(">",s)
     os.system(s)
+
+shutil.rmtree("PickleShareNet/bin")
+shutil.rmtree("PickleShareNet/obj")
 
 os.chdir("PickleShareNet")
 c("dotnet pack -c Release")
